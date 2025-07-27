98e02c648d5915ed567fcba37d80cf3e9534fcf5
ShreyaManikeri <shreyamanikeri@gmail.com>
2025-07-27 22:01:27 +0530

    Add files via upload

--- New.java
+++ New.java
@@ -1,12 +1,17 @@
 public class New {
-    int a=200;
-    int b=100;
-    public void m1(int a ,int b){
-   System.out.println(a+b);
-   System.out.println(this.a+this.b);
-    }
     public static void main(String[] args) {
-        New t=new New();
-        t.m1(10,20);
-    }
-}
+        String s="shreya";
+        System.out.println(s.charAt(3));
+        System.out.println(s.codePointAt(5));
+        System.out.println(s.codePointBefore(5));
+        System.out.println(s.compareTo("sg"));
+        System.out.println(s.compareTo("sg"));
+        String sw="shreya";
+        String sc="maya";
+        System.out.println(sw.equals(sc));
+        System.out.println(sw==s);
+        String s1=new String("me");
+        String s2=new String("me");
+        System.out.println(s1.equals(s2));
+        System.out.println(s1==s2);
+    }}
